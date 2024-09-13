19f488095744186a569dbd1d65b2e5c97bfccf6e
Antony-Rya <159479364+Antony-Rya@users.noreply.github.com>
2024-09-13 20:21:07 +0000

    Barras

--- grafico.py
+++ grafico.py
@@ -3,9 +3,10 @@ import pandas as pd
 mpl.use('Agg')
 import matplotlib.pyplot as plt
 df = pd.read_csv('informacoes.csv')
-eixoX = df['Disciplina']
-eixoY = df['Nota']
-
+eixoX = df['Nome do Aluno']
+eixoY = df['Média']
+fig, ax = plt.subplots(figsize=(8, 10))
 plt.title("Notas do aluno")
 plt.bar(eixoX, eixoY)
+plt.xticks(rotation=45, ha='right')
 plt.savefig('imagem.png')
